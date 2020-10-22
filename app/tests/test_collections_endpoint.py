8f862f22bcfcebc42f023ad26aa8772f27faae3d
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2020-10-22 15:56:39 +0200

    moved unit tests to new directory

--- app/tests/test_collections_endpoint.py
+++ app/tests/test_collections_endpoint.py
@@ -8,7 +8,9 @@ from stac_api.models import Keyword
 from stac_api.models import Link
 from stac_api.models import Provider
 from stac_api.models import get_default_stac_extensions
-from stac_api.serializers import DetailViewCollectionSerializer
+from stac_api.serializers import CollectionSerializer
+
+from config.settings import API_BASE_PATH
 
 
 class CollectionsEndpointTestCase(TestCase):  # pylint: disable = too-many-instance-attributes
@@ -170,14 +172,11 @@ class CollectionsEndpointTestCase(TestCase):  # pylint: disable = too-many-insta
         self.link3.save()
 
         # transate to Python native:
-        self.serializer = DetailViewCollectionSerializer(self.collection1)
+        self.serializer = CollectionSerializer(self.collection1)
 
     def test_collections_endpoint(self):
-        response = self.client.get("/collections/a_123/?format=json")
-        print(response.data)
+        response = self.client.get(f"/{API_BASE_PATH}collections/a_123/?format=json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             response.data, self.serializer.data, msg="Returned data does not match expected data"
         )
-        #response = self.client.get("/collections/?format=json")
-        #self.assertEqual(response.status_code, 200)
