c47f296b7d816c8ca0eeca19d4370ec1d718cfba
Christoph Boecklin <christoph.boecklin@swisstopo.ch>
2020-10-22 14:53:37 +0200

    removing print statements

--- app/middleware/debug_toolbar.py
+++ app/middleware/debug_toolbar.py
@@ -7,9 +7,8 @@ def check_toolbar_env(request):
     for details see
     https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config  # pylint: disable=line-too-long
     """
-    print(os.environ.get('APP_ENV'))
+
     if os.environ.get('APP_ENV', 'prod') in ['local', 'dev']:
-        print('blublu')
         return True
 
     return False
