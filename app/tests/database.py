2ae1dddd46e79c6ebe9210beaf5d7d8d92268abb
Brice Schaffner <brice.schaffner@swisstopo.ch>
2020-10-29 19:41:08 +0100

    BGDIINF_SB-1362: Improved test code and added items endpoint test
    Instead of redoing the db initialization in each test, moved the code in
    a package, database.py and reused it in tests.
    
    Added items endpoints test and also the collections list endpoint test.

---
+++ app/tests/database.py
@@ -0,0 +1,139 @@
+from stac_api.models import Asset
+from stac_api.models import Collection
+from stac_api.models import CollectionLink
+from stac_api.models import Item
+from stac_api.models import ItemLink
+from stac_api.models import Keyword
+from stac_api.models import Provider
+from stac_api.models import get_default_stac_extensions
+
+
+def create_collection():
+    '''Create a dummy collection db object for testing
+    '''
+    collection = Collection.objects.create(
+        crs=["http://www.opengis.net/def/crs/OGC/1.3/CRS84"],
+        created='2020-10-28T13:05:10.473602Z',
+        updated='2020-10-28T13:05:10.473602Z',
+        description='This is a description',
+        start_date=None,
+        end_date=None,
+        extent=[200000, 100000, 200001, 100005],
+        collection_name='ch.swisstopo.pixelkarte-farbe-pk200.noscale',
+        item_type='Feature',
+        license='test',
+        stac_extension=get_default_stac_extensions(),
+        stac_version="0.9.0",
+        summaries_eo_gsd=[10.1, 20.3, 30.44],
+        summaries_proj=[1, 4, 22],
+        geoadmin_variant=['blubb', 'blabb', 'blibb'],
+        title='Test title'
+    )
+    collection.save()
+
+    # create keyword instance for testing
+    keyword1 = Keyword.objects.create(name='test1')
+    keyword1.save()
+    collection.keywords.add(keyword1)
+    keyword2 = Keyword.objects.create(name='test2')
+    keyword2.save()
+    collection.keywords.add(keyword2)
+
+    # create provider instance for testing
+    provider = Provider.objects.create(
+        name='provider1',
+        description='description',
+        roles=['licensor'],
+        url='http://www.google.com'
+    )
+    provider.save()
+    collection.providers.add(provider)
+
+    # Create link
+    link = CollectionLink.objects.create(
+        collection=collection,
+        href='http://www.google.com',
+        rel='rel',
+        link_type='root',
+        title='Test title'
+    )
+    link.save()
+    collection.save()
+    keyword1.save()
+    keyword2.save()
+    provider.save()
+
+    return collection
+
+
+def create_item(collection):
+    '''Create a dummy item db object for testing
+    '''
+    # create item instance for testing
+    item = Item.objects.create(
+        collection=collection,
+        item_name='item-for-test',
+        properties_datetime='2020-10-28T13:05:10.473602Z',
+        properties_eo_gsd=[10, 30],
+        properties_title="My Title",
+        stac_extensions=get_default_stac_extensions(),
+        stac_version="0.9.0"
+    )
+    item.save()
+    create_item_links(item)
+    assets = create_assets(collection, item)
+    item.save()
+    return item, assets
+
+
+def create_item_links(item):
+    # create links instances for testing
+    link_root = ItemLink.objects.create(
+        item=item,
+        href="https://data.geo.admin.ch/api/stac/v0.9/",
+        rel='root',
+        link_type='root',
+        title='Root link'
+    )
+    link_self = ItemLink.objects.create(
+        item=item,
+        href=
+        "https://data.geo.admin.ch/collections/ch.swisstopo.pixelkarte-farbe-pk50.noscale/items/smr50-263-2016",
+        rel='self',
+        link_type='self',
+        title='Self link'
+    )
+    link_rel = ItemLink.objects.create(
+        item=item,
+        href=
+        "https://data.geo.admin.ch/api/stac/v0.9/collections/ch.swisstopo.pixelkarte-farbe-pk50.noscale",
+        rel='rel',
+        link_type='rel',
+        title='Rel link'
+    )
+    link_root.save()
+    link_self.save()
+    link_rel.save()
+    item.save()
+    return [link_root, link_self, link_rel]
+
+
+def create_assets(collection, item):
+    asset = Asset.objects.create(
+        collection=collection,
+        feature=item,
+        asset_name="my first asset",
+        checksum_multihash="01205c3fd6978a7d0b051efaa4263a09",
+        description="this an asset",
+        eo_gsd=3.4,
+        geoadmin_lang='fr',
+        geoadmin_variant="kgrs",
+        proj_epsq=2056,
+        media_type="image/tiff; application=geotiff; profile=cloud-optimize",
+        href=
+        "https://data.geo.admin.ch/ch.swisstopo.pixelkarte-farbe-pk50.noscale/smr200-200-1-2019-2056-kgrs-10.tiff"
+    )
+    asset.save()
+    item.save()
+    collection.save()
+    return [asset]
--- app/tests/test_serializer.py
+++ app/tests/test_serializer.py
@@ -1,7 +1,6 @@
 import io
 import logging
 from collections import OrderedDict
-from datetime import datetime
 from pprint import pformat
 
 from django.test import TestCase
@@ -10,16 +9,10 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework_gis.fields import GeoJsonDict
 
-from stac_api.models import Asset
-from stac_api.models import Collection
-from stac_api.models import CollectionLink
-from stac_api.models import Item
-from stac_api.models import ItemLink
-from stac_api.models import Keyword
-from stac_api.models import Provider
-from stac_api.models import get_default_stac_extensions
 from stac_api.serializers import CollectionSerializer
 from stac_api.serializers import ItemSerializer
+
+import tests.database as db
 
 logger = logging.getLogger(__name__)
 
@@ -31,121 +24,8 @@ class SerializationTestCase(TestCase):
         Prepare instances of keyword, link, provider and instance for testing.
         Adding the relationships among those by populating the ManyToMany fields
         '''
-        # create keyword instance for testing
-        self.keyword = Keyword.objects.create(name='test1')
-        self.keyword.save()
-
-        # create provider instance for testing
-        self.provider = Provider.objects.create(
-            name='provider1',
-            description='description',
-            roles=['licensor'],
-            url='http://www.google.com'
-        )
-        self.provider.save()
-
-        # create collection instance for testing
-        self.collection = Collection.objects.create(
-            id=1,
-            crs=['http://www.google.com'],
-            created=datetime.now(),
-            updated=datetime.now(),
-            description='desc',
-            start_date=None,
-            end_date=None,
-            extent=[200000, 100000, 200001, 100005],
-            collection_name='my collection',
-            item_type='Feature',
-            license='test',
-            stac_extension=get_default_stac_extensions(),
-            stac_version="0.9.0",
-            summaries_eo_gsd=[10.1, 20.3, 30.44],
-            summaries_proj=[1, 4, 22],
-            geoadmin_variant=['blubb', 'blabb', 'blibb'],
-            title='Test title'
-        )
-        self.collection.save()
-
-        # create links instance for testing
-        link = CollectionLink.objects.create(
-            collection=self.collection,
-            href='http://www.google.com',
-            rel='rel',
-            link_type='root',
-            title='Test title'
-        )
-        link.save()
-
-        # populate the ManyToMany relation fields
-        self.collection.keywords.add(self.keyword)
-        self.collection.providers.add(self.provider)
-
-        # create item instance for testing
-        self.item = Item.objects.create(
-            collection=self.collection,
-            item_name='item-for-test',
-            properties_datetime='2020-10-28T13:05:10.473602Z',
-            properties_eo_gsd=[10, 30],
-            properties_title="My Title",
-            stac_extensions=get_default_stac_extensions(),
-            stac_version="0.9.0"
-        )
-        self.item.save()
-        self._create_item_links()
-        self._create_assets()
-
-        # save the updated instances
-        self.collection.save()
-        self.keyword.save()
-        self.provider.save()
-
-    def _create_item_links(self):
-        # create links instances for testing
-        link_root = ItemLink.objects.create(
-            item=self.item,
-            href="https://data.geo.admin.ch/api/stac/v0.9/",
-            rel='root',
-            link_type='root',
-            title='Root link'
-        )
-        link_self = ItemLink.objects.create(
-            item=self.item,
-            href=
-            "https://data.geo.admin.ch/collections/ch.swisstopo.pixelkarte-farbe-pk50.noscale/items/smr50-263-2016",
-            rel='self',
-            link_type='self',
-            title='Self link'
-        )
-        link_rel = ItemLink.objects.create(
-            item=self.item,
-            href=
-            "https://data.geo.admin.ch/api/stac/v0.9/collections/ch.swisstopo.pixelkarte-farbe-pk50.noscale",
-            rel='rel',
-            link_type='rel',
-            title='Rel link'
-        )
-        link_root.save()
-        link_self.save()
-        link_rel.save()
-        self.item.save()
-
-    def _create_assets(self):
-        asset = Asset.objects.create(
-            collection=self.collection,
-            feature=self.item,
-            asset_name="my first asset",
-            checksum_multihash="01205c3fd6978a7d0b051efaa4263a09",
-            description="this an asset",
-            eo_gsd=3.4,
-            geoadmin_lang='fr',
-            geoadmin_variant="kgrs",
-            proj_epsq=2056,
-            media_type="image/tiff; application=geotiff; profile=cloud-optimize",
-            href=
-            "https://data.geo.admin.ch/ch.swisstopo.pixelkarte-farbe-pk50.noscale/smr200-200-1-2019-2056-kgrs-10.tiff"
-        )
-        asset.save()
-        self.item.save()
+        self.collection = db.create_collection()
+        self.item, self.assets = db.create_item(self.collection)
 
     def test_collection_serialization(self):
 
@@ -189,7 +69,7 @@ class SerializationTestCase(TestCase):
         # yapf: disable
         self.assertDictContainsSubset({
             'id': 'item-for-test',
-            'collection': 'my collection',
+            'collection': 'ch.swisstopo.pixelkarte-farbe-pk200.noscale',
             'type': 'Feature',
             'stac_version': '0.9.0',
             'geometry': GeoJsonDict([
