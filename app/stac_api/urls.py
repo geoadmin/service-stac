2bf55022069ea14597340dd7dc5c39cf0402422c
Brice Schaffner <brice.schaffner@swisstopo.ch>
2020-10-29 15:22:46 +0100

    BGDIINF_SB-1362: Added Items and Item endpoint and corrected the Collections endpoint
    Moved the collection view back to views.py as it was internal discussed.
    
    In the collections endpoint encapsulate the list into an object in the
    'collections' key as defined in the spec.
    
    Fixed a bug in the collection endpoint where the collection name is a
    string and not a slug. Slug doesn't allow special characters like `.`
    which might be part of the collection name.
    
    Added Items and Item endpoint.

--- app/stac_api/urls.py
+++ app/stac_api/urls.py
@@ -1,18 +1,32 @@
+from django.conf import settings
 from django.urls import path
 
-from config.settings import API_BASE_PATH
 from stac_api.views import CollectionDetail
 from stac_api.views import CollectionList
+from stac_api.views import ItemDetail
+from stac_api.views import ItemsList
 
 from . import views
 
+API_BASE = settings.API_BASE_PATH
+
 urlpatterns = [
-    path(API_BASE_PATH, views.landing_page, name='landing-page'),
+    path(API_BASE, views.landing_page, name='landing-page'),
     path('checker/', views.checker, name='checker'),
-    path(f"{API_BASE_PATH}collections/", CollectionList.as_view(), name='collection-list'),
+    path(f"{API_BASE}collections/", CollectionList.as_view(), name='collection-list'),
     path(
-        f"{API_BASE_PATH}collections/<slug:collection_name>/",
+        f"{API_BASE}collections/<str:collection_name>/",
         CollectionDetail.as_view(),
         name='collection-detail'
+    ),
+    path(
+        f"{API_BASE}collections/<str:collection_name>/items",
+        ItemsList.as_view(),
+        name='items-list'
+    ),
+    path(
+        f"{API_BASE}collections/<str:collection_name>/items/<str:item_name>/",
+        ItemDetail.as_view(),
+        name='item-detail'
     ),
 ]
