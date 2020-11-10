9f8e9d34cc78ddfe0155e7e6f8f540b14ac16b78
Brice Schaffner <brice.schaffner@swisstopo.ch>
2020-11-10 07:27:15 +0100

    BGDIINF_SB-1363: complete sample test and fix sample issues
    Some sample had issue that did not match the spec and generated output,
    therefore the sample has been updated.

--- app/tests/test_sampledata.py
+++ app/tests/test_sampledata.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 from pathlib import Path
+from pprint import pformat
 
 from django.conf import settings
 from django.test import Client
@@ -36,18 +37,45 @@ class SampleDataTestCase(TestCase):
 
         response = self.client.get(f"/{API_BASE}collections/{collection.collection_name}")
         payload = response.json()
+        logger.debug('Payload:\n%s', pformat(payload))
         self.assertEqual(200, response.status_code)
 
-        self._test_dict(collection_dict, payload)
+        self._test_dict('', collection_dict, payload)
 
-    def _test_dict(self, dct, payload):
+    def _test_dict(self, parent_path, dct, payload):
         for key, value in dct.items():
-            self.assertIn(key, payload, msg=f'Key {key} is not in payload')
+            path = f'{parent_path}[{key}]'
+            self.assertIn(key, payload, msg=f'{parent_path}: Key {key} is not in payload')
+            self.assertEqual(
+                type(value),
+                type(payload[key]),
+                msg=f'{parent_path}: key {key} type does not match'
+            )
+            if key in ['stac_extensions', 'spatial', 'temporal']:
+                # remove this if when all parts are fully implemented.
+                # See BGDIINF_SB-1410, BGDIINF_SB-1427 and BGDIINF_SB-1429
+                logger.warning('%s: Ignore key %s check', parent_path, key)
+                continue
             if isinstance(value, dict):
-                self._test_dict(value, payload[key])
+                self._test_dict(path, value, payload[key])
             elif isinstance(value, list):
-                pass
-            elif key == 'stac_extensions':
-                pass
+                if key in ['geoadmin:variant']:
+                    self._test_list(path, sorted(value), sorted(payload[key]))
+                else:
+                    self._test_list(path, value, payload[key])
             else:
-                self.assertEqual(str(value), payload[key], msg=f'Key {key} is not equal in payload')
+                self.assertEqual(
+                    value, payload[key], msg=f'{parent_path}: Key {key} is not equal in payload'
+                )
+
+    def _test_list(self, parent_path, lst, payload):
+        for i, value in enumerate(lst):
+            path = f'{parent_path}[{i}]'
+            if isinstance(value, dict):
+                self._test_dict(path, value, payload[i])
+            elif isinstance(value, list):
+                self._test_list(path, value, payload[i])
+            else:
+                self.assertEqual(
+                    value, payload[i], msg=f'{parent_path}: List index {i} is not equal in payload'
+                )
