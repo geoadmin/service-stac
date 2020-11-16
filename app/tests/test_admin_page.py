5901dc84aea81358ae41fd057f6eabc7c50ec18e
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2020-11-16 11:23:05 +0100

    removed debug print

--- app/tests/test_admin_page.py
+++ app/tests/test_admin_page.py
@@ -1,6 +1,7 @@
 from django.test import Client
 from django.test import TestCase
 
+
 class AdminTestCase(TestCase):
 
     def setUp(self):
@@ -8,6 +9,5 @@ class AdminTestCase(TestCase):
 
     def test_admin_page(self):
         # very simple test to check if the admin page login is up
-        response = self.client.get(f"/api/stac/admin/login/?next=/api/stac/admin")
-        print(response)
+        response = self.client.get("/api/stac/admin/login/?next=/api/stac/admin")
         self.assertEqual(response.status_code, 200)
