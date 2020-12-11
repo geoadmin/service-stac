79f7dbab5722c01a190f3e31b1224f900972d7d9
hansmannj <Juergen.Hansmann@swisstopo.ch>
2020-12-11 10:25:07 +0100

    BGDIINF_SB-1491 unit test get_token-endpoint
    * added unit test for the get_token endpoint requesting token with wrong credentials
    * changed error message in case status code is not 200 when requesting token with correct credentials

--- app/tests/test_get_token_endpoint.py
+++ app/tests/test_get_token_endpoint.py
@@ -7,6 +7,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from rest_framework.test import APIRequestFactory
 
+from tests.utils import get_http_error_description
+
 API_BASE = settings.API_BASE
 
 
@@ -25,11 +27,18 @@ class GetTokenEndpointTestCase(TestCase):
     def test_get_token_with_valid_credentials(self):
         url = reverse('get_token')
         response = self.client.post(url, {'username': self.username, 'password': self.password})
-        self.assertEqual(200, response.status_code, msg="User request for token failed.")
+        self.assertEqual(200, response.status_code, msg=get_http_error_description(response.json()))
         generated_token = response.data["token"]
         token_from_db = Token.objects.get(user=self.user)
         self.assertEqual(
             generated_token,
             token_from_db.key,
             msg="Generated token and token stored in DB do not match."
+        )
+
+    def test_get_token_with_invalid_credentials(self):
+        url = reverse('get_token')
+        response = self.client.post(url, {'username': self.username, 'password': 'wrong_password'})
+        self.assertEqual(
+            400, response.status_code, msg="Token for unauthorized user has been created."
         )
