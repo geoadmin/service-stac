53c3e4e78bb519db73c8885556faad4579c55d52
Marc Monnerat <procrastinatio@gmail.com>
2021-01-14 17:07:51 +0100

    linting, formatting and a basic test

--- app/tests/test_collections_model.py
+++ app/tests/test_collections_model.py
@@ -1,8 +1,25 @@
 import logging
-from datetime import datetime
 
+from django.contrib.gis.geos import GEOSGeometry
 from django.test import TestCase
 
-from tests.data_factory import Factory
+from stac_api.models import Collection
 
 logger = logging.getLogger(__name__)
+
+
+class CollectionsModelTestCase(TestCase):
+
+    def test_collection_create_model(self):
+        collection = Collection(
+            name='collection-1',
+            extent_geometry=GEOSGeometry(
+                'SRID=4326;POLYGON '
+                '((5.96 45.82, 5.96 47.81, 10.49 47.81, 10.49 45.82, 5.96 45.82))'
+            ),
+            description='cool collection',
+            license='WTFPL'
+        )
+        collection.full_clean()
+        collection.save()
+        self.assertEqual('collection-1', collection.name)
