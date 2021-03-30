28887d56bb742ba5d9c3bfbd41c7543991152271
Tobias Reber <tobias.reber@swisstopo.ch>
2021-03-30 11:08:36 +0200

    BGDIINF_SB-1740: Uncommented delete asset on S3

--- app/stac_api/signals.py
+++ app/stac_api/signals.py
@@ -1,17 +1,18 @@
 # Un-comment with BGDIINF_SB-1625
-# import logging
+import logging
 
-# from django.db.models.signals import pre_delete
-# from django.dispatch import receiver
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
-# from stac_api.models import Asset
+from stac_api.models import Asset
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
-# @receiver(pre_delete, sender=Asset)
-# def delete_s3_asset(sender, instance, **kwargs):
-#     # The file is not automatically deleted by Django
-#     # when the object holding its reference is deleted
-#     # hence it has to be done here.
-#     logger.info("The asset %s is deleted from s3", instance.file.name)
-#     instance.file.delete(save=False)
+
+@receiver(pre_delete, sender=Asset)
+def delete_s3_asset(sender, instance, **kwargs):
+    # The file is not automatically deleted by Django
+    # when the object holding its reference is deleted
+    # hence it has to be done here.
+    logger.info("The asset %s is deleted from s3", instance.file.name)
+    instance.file.delete(save=False)
