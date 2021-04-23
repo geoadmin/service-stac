6b72c413e056ae53673b4bacb77cccc512a84f62
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2021-04-23 11:31:25 +0200

    BGDIINF_SB-1753 added unit test
    * added unit tests asserting collections updated property gets correctly updated on
      item creation, item update, item delete, assets creation, assets update and
      assets deletion

--- app/tests/test_collection_model.py
+++ app/tests/test_collection_model.py
@@ -1,11 +1,13 @@
 import logging
+from time import sleep
 
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from stac_api.models import Collection
 
-from tests.data_factory import CollectionFactory
+from tests.data_factory import Factory
+from tests.utils import mock_s3_asset_file
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +15,10 @@ logger = logging.getLogger(__name__)
 class CollectionsModelTestCase(TestCase):
 
     @classmethod
+    @mock_s3_asset_file
     def setUpTestData(cls):
-        cls.collection = CollectionFactory().create_sample(db_create=True)
+        cls.factory = Factory()
+        cls.collection = Factory().create_collection_sample(db_create=True)
 
     def test_create_already_existing_collection(self):
         # try to create already existing collection twice
@@ -26,14 +30,14 @@ class CollectionsModelTestCase(TestCase):
     def test_create_collection_invalid_name(self):
         # try to create a collection with invalid collection name
         with self.assertRaises(ValidationError, msg="Collection with invalid name was accepted."):
-            CollectionFactory().create_sample(sample="collection-invalid", db_create=True)
+            Factory().create_collection_sample(sample="collection-invalid", db_create=True)
 
     def test_create_collection_missing_mandatory_fields(self):
         # try to create a collection with invalid collection name
         with self.assertRaises(
             ValidationError, msg="Collection with missing mandatory fields was accepted."
         ):
-            CollectionFactory().create_sample(
+            Factory().create_collection_sample(
                 name="collection-missing-mandatory-fields",
                 sample="collection-missing-mandatory-fields",
                 db_create=True
@@ -42,7 +46,7 @@ class CollectionsModelTestCase(TestCase):
     def test_create_collection_invalid_links(self):
         # try to create a collection with invalid collection name
         with self.assertRaises(ValidationError, msg="Collection with invalid links was accepted."):
-            CollectionFactory().create_sample(
+            Factory().create_collection_sample(
                 name="collection-invalid-links", sample="collection-invalid-links", db_create=True
             )
 
@@ -51,20 +55,105 @@ class CollectionsModelTestCase(TestCase):
         with self.assertRaises(
             ValidationError, msg="Collection with invalid providers was accepted."
         ):
-            CollectionFactory().create_sample(sample="collection-invalid-providers", db_create=True)
+            Factory().create_collection_sample(
+                sample="collection-invalid-providers", db_create=True
+            )
 
     def test_create_collection_with_providers_and_links(self):
         # try to create a valid collection with providers and links. Should not raise any errors.
-        CollectionFactory().create_sample(
+        Factory().create_collection_sample(
             name="collection-links-providers", sample="collection-1", db_create=True
         )
 
     def test_create_collection_only_required_attributes(self):
         # try to create a valid collection with only the required attributes.
         # Should not raise any errors.
-        CollectionFactory().create_sample(
+        Factory().create_collection_sample(
             name="collection-required-only",
             sample="collection-1",
             db_create=True,
             required_only=True
+        )
+
+    def test_collection_update_on_item_write_operations(self):
+        # assert that collection's updated property is updated when an item is
+        # added to the collection, this item is updated and this item is deleted
+
+        # check collection's update on item insertion
+        initial_last_modified = self.collection.model.updated
+        sleep(0.01)
+        item = Factory().create_item_sample(self.collection.model, sample='item-1', db_create=True)
+        item.model.full_clean()
+        item.model.save()
+        self.collection.model.refresh_from_db()
+        self.assertGreater(
+            self.collection.model.updated,
+            initial_last_modified,
+            msg="Collection's updated property was not updated on item insert"
+        )
+
+        # check collection's update on item update
+        initial_last_modified = self.collection.model.updated
+        sleep(0.01)
+        item.model.name = "new_randon_name"
+        item.model.full_clean()
+        item.model.save()
+        self.collection.model.refresh_from_db()
+        self.assertGreater(
+            self.collection.model.updated,
+            initial_last_modified,
+            msg="Collection's updated property was not updated on item update"
+        )
+
+        # check collection's update on item deletion
+        initial_last_modified = self.collection.model.updated
+        sleep(0.01)
+        item.model.delete()
+        self.collection.model.refresh_from_db()
+        self.assertGreater(
+            self.collection.model.updated,
+            initial_last_modified,
+            msg="Collection's updated property was not updated on item deletion"
+        )
+
+    def test_collection_update_on_asset_write_operations(self):
+        # assert that collection's updated property is updated when an asset is
+        # added to an item of the collection, this asset is updated and this asset is deleted
+
+        # check collection's update on asset insertion
+        item = Factory().create_item_sample(self.collection.model, sample='item-1', db_create=True)
+        initial_last_modified = self.collection.model.updated
+        sleep(0.01)
+        asset = Factory().create_asset_sample(item=item.model, sample='asset-1', db_create=True)
+        asset.model.full_clean()
+        asset.model.save()
+        self.collection.model.refresh_from_db()
+        self.assertGreater(
+            self.collection.model.updated,
+            initial_last_modified,
+            msg="Collection's updated property was not updated on asset insert"
+        )
+
+        # check collection's update on asset update
+        initial_last_modified = self.collection.model.updated
+        sleep(0.01)
+        asset.model.name = f"new-{asset.model.name}"
+        asset.model.full_clean()
+        asset.model.save()
+        self.collection.model.refresh_from_db()
+        self.assertGreater(
+            self.collection.model.updated,
+            initial_last_modified,
+            msg="Collection's updated property was not updated on asset update"
+        )
+
+        # check collection's update on asset deletion
+        initial_last_modified = self.collection.model.updated
+        sleep(0.01)
+        asset.model.delete()
+        self.collection.model.refresh_from_db()
+        self.assertGreater(
+            self.collection.model.updated,
+            initial_last_modified,
+            msg="Collection's updated property was not updated on asset deletion"
         )
