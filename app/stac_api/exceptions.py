ecd3198c966a7d87efb8731004a27983f65e8ece
Brice Schaffner <brice.schaffner@swisstopo.ch>
2023-07-05 14:33:09 +0200

    BGDIINF_SB-2971: Changed the HTTP response to 409 in case of duplicate asset upload
    Now in case of a duplicate asset upload, it returns a 409 - conflict with the
    current upload id in the answer.

---
+++ app/stac_api/exceptions.py
@@ -0,0 +1,34 @@
+import logging
+
+from django.utils.translation import gettext_lazy as _
+
+import rest_framework.exceptions
+from rest_framework import status
+
+logger = logging.getLogger(__name__)
+
+
+class StacAPIException(rest_framework.exceptions.APIException):
+    '''STAC API custom exception
+
+    These exception can add additional data to the HTTP response.
+    '''
+
+    def __init__(self, detail=None, code=None, data=None):
+        super().__init__(detail, code)
+        if isinstance(data, dict):
+            self.data = data
+        elif data:
+            self.data = {'data': data}
+
+
+class UploadNotInProgressError(StacAPIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = _('Upload not in progress')
+    default_code = 'conflict'
+
+
+class UploadInProgressError(StacAPIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = _('Upload in progress')
+    default_code = 'conflict'
