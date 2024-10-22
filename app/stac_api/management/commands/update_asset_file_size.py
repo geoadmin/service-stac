8c9e9f8808fa3a1de067d625d4ea9cc9d2c83238
Lukas Joss <lukas.joss@swisstopo.ch>
2024-10-22 15:58:15 +0200

    PB-917: Update script to choose correct bucket

--- app/stac_api/management/commands/update_asset_file_size.py
+++ app/stac_api/management/commands/update_asset_file_size.py
@@ -7,13 +7,11 @@ from django.core.management.base import BaseCommand
 
 from stac_api.models import Asset
 from stac_api.models import CollectionAsset
-from stac_api.utils import AVAILABLE_S3_BUCKETS
 from stac_api.utils import CommandHandler
 from stac_api.utils import get_s3_client
+from stac_api.utils import select_s3_bucket
 from stac_api.views.upload import SharedAssetUploadBase
 
-# path definition relative to the directory that contains manage.py
-DATADIR = settings.BASE_DIR / 'app/stac_api/sample_data/'
 logger = logging.getLogger(__name__)
 
 
@@ -21,12 +19,13 @@ class Handler(CommandHandler):
 
     def update(self):
         self.print_success('running command to update file size')
-        s3 = get_s3_client(AVAILABLE_S3_BUCKETS.legacy)
-        bucket = settings.AWS_SETTINGS[AVAILABLE_S3_BUCKETS.legacy.name]['S3_BUCKET_NAME']
 
         self.print_success('update file size for assets')
         assets = Asset.objects.filter(file_size=0).all()
         for asset in assets:
+            selected_bucket = select_s3_bucket(asset.item.collection.name)
+            s3 = get_s3_client(selected_bucket)
+            bucket = settings.AWS_SETTINGS[selected_bucket.name]['S3_BUCKET_NAME']
             key = SharedAssetUploadBase.get_path(None, asset)
             try:
                 file_size = s3.head_object(Bucket=bucket, Key=key)['ContentLength']
@@ -38,6 +37,9 @@ class Handler(CommandHandler):
         self.print_success('update file size for collection assets')
         collection_assets = CollectionAsset.objects.filter(file_size=0).all()
         for collection_asset in collection_assets:
+            selected_bucket = select_s3_bucket(collection_asset.collection.name)
+            s3 = get_s3_client(selected_bucket)
+            bucket = settings.AWS_SETTINGS[selected_bucket.name]['S3_BUCKET_NAME']
             key = SharedAssetUploadBase.get_path(None, collection_asset)
             try:
                 file_size = s3.head_object(Bucket=bucket, Key=key)['ContentLength']
