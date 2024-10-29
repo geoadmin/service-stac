c9d7bb12b2bdbf3873d1750065ca3601d80ee28c
Lukas Joss <lukas.joss@swisstopo.ch>
2024-10-29 14:30:25 +0100

    PB-987: Add migration reverse

--- app/stac_api/migrations/0053_update_conformance_endpoint.py
+++ app/stac_api/migrations/0053_update_conformance_endpoint.py
@@ -10,9 +10,17 @@ def add_landing_page_version(apps, schema_editor):
     lp.save()
 
 
+def reverse_landing_page_version(apps, schema_editor):
+    # Remove the landing page v0.9
+    LandingPage = apps.get_model("stac_api", "LandingPage")
+    lp = LandingPage.objects.get(version='v1')
+    lp.conformsTo.remove('https://api.stacspec.org/v1.0.0/ogcapi-features')
+    lp.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("stac_api", "0052_asset_add_del_asset_item_file_size_trigger_and_more"),
     ]
 
-    operations = [migrations.RunPython(add_landing_page_version)]
+    operations = [migrations.RunPython(add_landing_page_version, reverse_landing_page_version)]
