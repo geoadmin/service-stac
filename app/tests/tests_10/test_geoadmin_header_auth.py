d90b71fcb6e6ec6ed630533bf8758dfd29f68ea8
Adrien Kunysz <adrien.kunysz@swisstopo.ch>
2025-01-23 09:47:19 +0100

    PB-1366: make new auth work for write requests.
    Django is configured to use CSRF mitigation tokens. This only works for clients
    that are web browsers. Typical API clients are not web browsers, do not keep
    track of cookies and fail CSRF mitigation checks. The old token authentication
    method works around that by disabling CSRF mitigation when that authentication
    method is used. With this change, we do the same for the new token authentication
    method.
    
    In PB-1009 we implemented the new authentication method for the Admin UI.
    This change does the same for the API endpoints and add tests exercising it.
    
    The API endpoints are rest_framework views that rely on authentication
    methods defined by the REST framework which is different from the Admin UI
    authentication plumbing. To support both, we need to have modules for both
    even though they basically do the same thing.
    
    Specifically:
    * create `api_gateway_authentication` as `rest_framework` authentication module
    * create `api_gateway_middleware` as generic Django authentication module (for Admin UI)
    * abstract the common parts into a new `api_gateway` module
    * configure both authentication modules in the debug build
    * add tests exercising a PUT API endpoint for both v0.9 and v1, including CSRF checks
    
    This is all flag-guarded behind FEATURE_AUTH_ENABLE_APIGW.

---
+++ app/tests/tests_10/test_geoadmin_header_auth.py
@@ -0,0 +1,51 @@
+import logging
+
+from parameterized import parameterized
+
+from django.contrib.auth import get_user_model
+from django.test import Client
+from django.test import override_settings
+
+from tests.tests_10.base_test import STAC_BASE_V
+from tests.tests_10.base_test import StacBaseTestCase
+from tests.tests_10.data_factory import Factory
+
+logger = logging.getLogger(__name__)
+
+
+@override_settings(FEATURE_AUTH_ENABLE_APIGW=True)
+class GeoadminHeadersAuthForPutEndpointTestCase(StacBaseTestCase):
+    valid_username = "another_test_user"
+
+    def setUp(self):  # pylint: disable=invalid-name
+        self.client = Client(enforce_csrf_checks=True)
+        self.factory = Factory()
+        self.collection = self.factory.create_collection_sample()
+        get_user_model().objects.create_superuser(self.valid_username)
+
+    @parameterized.expand([
+        (valid_username, "true", 201),
+        (None, None, 401),
+        (valid_username, "false", 401),
+        ("wronguser", "true", 403),
+    ])
+    def test_collection_upsert_create_with_geoadmin_header_auth(
+        self, username_header, authenticated_header, expected_response_code
+    ):
+        sample = self.factory.create_collection_sample(sample='collection-2')
+
+        headers = None
+        if username_header or authenticated_header:
+            headers = {
+                "Geoadmin-Username": username_header,
+                "Geoadmin-Authenticated": authenticated_header,
+            }
+        response = self.client.put(
+            path=f"/{STAC_BASE_V}/collections/{sample['name']}",
+            data=sample.get_json('put'),
+            content_type='application/json',
+            headers=headers,
+        )
+        self.assertStatusCode(expected_response_code, response)
+        if 200 <= expected_response_code < 300:
+            self.check_stac_collection(sample.json, response.json())
