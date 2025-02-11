19f63d6a74fc12cffaf69efcffb60c7df91f03ba
Alexander Steiner <alexander.steiner@swisstopo.ch>
2025-02-11 11:59:41 +0100

    PB-1279: Add GET request to set up session as a workaround

--- app/tests/tests_10/test_items_endpoint.py
+++ app/tests/tests_10/test_items_endpoint.py
@@ -953,6 +953,17 @@ class ItemsBulkCreateEndpointTestCase(StacBaseTestCase):
         collection_name = self.collection["name"]
         self.factory.create_item_sample(self.collection.model, sample='item-1', db_create=True)
 
+        # Perform a meaningless GET request in order to have a session from authentication.
+        #
+        # If we don't do this, the authentication middleware catches the HTTP 400
+        # error while trying to set up a session. The session initialization then
+        # fails with a HTML error message (not a JSON) complaining that
+        #
+        #     "The request's session was deleted before the request completed.".
+        #
+        # This is a workaround that we might be able to replace with a more proper solution.
+        self.client.get(path=f'/{STAC_BASE_V}/collections/{collection_name}/items')
+
         response = self.client.post(
             path=f'/{STAC_BASE_V}/collections/{collection_name}/items',
             data=self.payload,
