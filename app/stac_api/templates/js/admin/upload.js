fc331d5d1c6d393dc43f9050e4a0c69a7dce51c8
Adrien Kunysz <adrien.kunysz@swisstopo.ch>
2025-02-27 10:49:41 +0100

    PB-1469: use v0.9 for large uploads from admin UI.
    In PB-1229 we started routing all STAC traffic through API Gateway which restricts the size of requests.
    In PB-1331 we updated the admin UI upload page to use S3 URLs for upload such that large requests don't go through API Gateway any more. This uses v1 API endpoints from Javascript.
    In PB-1281 we disabled session authentication for v1 endpoints, thus breaking admin UI upload page.
    In PB-1380 we opened v1 in PROD.
    
    We prefer to not re-enable session authentication for v1 because disabling it later will be harder as (more) users would start relying on it.
    Current workaround involves uploading assets on behalf of users with our own script using the API.
    
    This change updates the Javascript to use v0.9 API endpoints which still accept session-based authentication.
    This is a temporary workaround until we figure out a better long term fix that does not rely on v0.9.

--- app/stac_api/templates/js/admin/upload.js
+++ app/stac_api/templates/js/admin/upload.js
@@ -36,7 +36,7 @@ function hashValue(val) {
 async function cleanUploadsInProgress() {
     setStatus('checking for uploads in progress');
     // Get uploads in progress
-    const url = `/api/stac/v1/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads?status=in-progress`;
+    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads?status=in-progress`;
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -60,7 +60,7 @@ async function cleanUploadsInProgress() {
 // Abort the upload by upload_id
 async function abortMultipartUpload(upload_id) {
     setStatus('aborting upload in progress');
-    const url = `/api/stac/v1/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads/${upload_id}/abort`;
+    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads/${upload_id}/abort`;
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -106,7 +106,7 @@ function handleFileFormSubmit() {
 // On success calls `uploadFile`.
 async function createPresigned(md5, multi, file) {
     setStatus(`Creating presigned url...`);
-    const url = `/api/stac/v1/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads`;
+    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads`;
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -168,7 +168,7 @@ async function uploadFile(data, file) {
 // On success redirects back to asset overview.
 async function completeMultipartUpload(data, etag) {
     setStatus(`Completing upload...`);
-    const url = `/api/stac/v1/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads/${data.upload_id}/complete`;
+    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads/${data.upload_id}/complete`;
     try {
         const response = await fetch(url, {
             method: 'POST',
