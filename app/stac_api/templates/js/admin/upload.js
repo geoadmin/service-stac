af8e1ee482dc07e17305a80f207a846b8f6c1a3a
Adrien Kunysz <adrien.kunysz@swisstopo.ch>
2025-02-27 12:17:07 +0100

    PB-1469: move the bulk of the URL string into a separate function.
    This avoids some repetition and makes it easier to switch between v0.9 and v1.

--- app/stac_api/templates/js/admin/upload.js
+++ app/stac_api/templates/js/admin/upload.js
@@ -10,6 +10,10 @@ function setStatus(text) {
     document.getElementById('current_status').textContent = text;
 }
 
+function getAssetUploadUrlPrefix(collection, item, asset) {
+    return `/api/stac/v0.9/collections/${collection}/items/${item}/assets/${asset}/uploads`
+}
+
 function setError(text) {
     document.getElementById('error_box').style.display = 'block';
     document.getElementById('error').textContent = text;
@@ -36,7 +40,7 @@ function hashValue(val) {
 async function cleanUploadsInProgress() {
     setStatus('checking for uploads in progress');
     // Get uploads in progress
-    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads?status=in-progress`;
+    const url = getAssetUploadUrlPrefix(window.collection_name, window.item_name, window.asset_name) + '?status=in-progress';
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -60,7 +64,7 @@ async function cleanUploadsInProgress() {
 // Abort the upload by upload_id
 async function abortMultipartUpload(upload_id) {
     setStatus('aborting upload in progress');
-    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads/${upload_id}/abort`;
+    const url = getAssetUploadUrlPrefix(window.collection_name, window.item_name, window.asset_name) + `/${upload_id}/abort`;
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -106,7 +110,7 @@ function handleFileFormSubmit() {
 // On success calls `uploadFile`.
 async function createPresigned(md5, multi, file) {
     setStatus(`Creating presigned url...`);
-    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads`;
+    const url = getAssetUploadUrlPrefix(window.collection_name, window.item_name, window.asset_name);
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -168,7 +172,7 @@ async function uploadFile(data, file) {
 // On success redirects back to asset overview.
 async function completeMultipartUpload(data, etag) {
     setStatus(`Completing upload...`);
-    const url = `/api/stac/v0.9/collections/${window.collection_name}/items/${window.item_name}/assets/${window.asset_name}/uploads/${data.upload_id}/complete`;
+    const url = getAssetUploadUrlPrefix(window.collection_name, window.item_name, window.asset_name) + `/${data.upload_id}/complete`;
     try {
         const response = await fetch(url, {
             method: 'POST',
