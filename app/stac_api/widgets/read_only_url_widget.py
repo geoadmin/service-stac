baf45847c156b9810a16514fe7cc37b7cf55dc45
Sami Nouidri <sami.nouidri@swisstopo.ch>
2025-03-05 08:53:54 +0100

    PB-1434 : removed URL formatting from custom widget

--- app/stac_api/widgets/read_only_url_widget.py
+++ app/stac_api/widgets/read_only_url_widget.py
@@ -2,10 +2,10 @@ from django import forms
 from django.utils.safestring import mark_safe
 
 
-class ReadOnlyURLWidget(forms.TextInput):
-    """Custom widget to display the file field as a clickable URL."""
+class LabelWidget(forms.TextInput):
+    """Custom widget to display a label without input"""
 
     def render(self, name, value, attrs=None, renderer=None):
         if value:
-            return mark_safe(f'Currently : <a href=" {value}" target="_blank">{value}</a>')
+            return mark_safe(f'{value}')
         return super().render(name, value, attrs, renderer)
