a0a72b2b6e8f548844fc23051ddbf281763ed986
Sami Nouidri <sami.nouidri@swisstopo.ch>
2025-03-20 16:29:46 +0100

    PB-1482 : Adjusting tests according to review comments

--- app/tests/tests_10/test_collection_assets_endpoint.py
+++ app/tests/tests_10/test_collection_assets_endpoint.py
@@ -5,6 +5,7 @@ from json import dumps
 from json import loads
 from pprint import pformat
 
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import Client
 from django.test import override_settings
@@ -16,7 +17,7 @@ from stac_api.models.collection import CollectionAsset
 from stac_api.utils import get_collection_asset_path
 from stac_api.utils import utc_aware
 
-from tests.tests_10.base_test import STAC_BASE_V
+from tests.tests_10.base_test import STAC_BASE_V, TEST_SERVER
 from tests.tests_10.base_test import StacBaseTestCase
 from tests.tests_10.base_test import StacBaseTransactionTestCase
 from tests.tests_10.data_factory import Factory
@@ -219,7 +220,7 @@ class CollectionAssetsCreateEndpointTestCase(StacBaseTestCase):
         # Check also, that the asset does not exist in the DB already
         self.assertFalse(
             CollectionAsset.objects.filter(name=asset_name).exists(),
-            msg="Deleted colelction asset still found in DB"
+            msg="Deleted collection asset still found in DB"
         )
 
         # Now use upsert to create the new asset
@@ -307,12 +308,24 @@ class CollectionAssetsUpdateEndpointAssetFileTestCase(StacBaseTestCase):
         self.asset = self.factory.create_collection_asset_sample(
             collection=self.collection.model, db_create=True
         )
+
+        self.collection_external = self.factory.create_collection_sample(db_create=True).model
+        self.collection_external.allow_external_assets = True
+        self.collection_external.external_asset_whitelist = [settings.EXTERNAL_TEST_ASSET_URL]
+        self.collection_external.save()
+        self.internal_asset = self.factory.create_collection_asset_sample(
+            collection=self.collection_external, sample='internal-asset', db_create=True
+        )
+        self.external_asset = self.factory.create_collection_asset_sample(
+            collection=self.collection_external, sample='external-asset', db_create=True
+        )
+
         self.client = Client(headers=get_auth_headers())
         self.maxDiff = None  # pylint: disable=invalid-name
 
-    def test_asset_endpoint_patch_put_href_to_external_href(self):
-        collection_name = self.collection['name']
-        asset_name = self.asset['name']
+    def test_asset_endpoint_put_patch_href_to_external_href(self):
+        collection_name = self.collection_external.name
+        asset_name = self.internal_asset['name']
         asset_sample = self.asset.copy()
 
         path = f'/{STAC_BASE_V}/collections/{collection_name}/assets/{asset_name}'
@@ -320,33 +333,43 @@ class CollectionAssetsUpdateEndpointAssetFileTestCase(StacBaseTestCase):
         #verify the asset is internal before patching it
         response = self.client.get(path)
         self.assertStatusCode(200, response)
-        self.assertFalse(response.json()['is_external'])
+        self.assertIn(TEST_SERVER, response.json()['href'])
 
         put_payload = asset_sample.get_json('put')
-        put_payload['href'] = 'https://testserver/external-asset'
-        patch_payload = {'href': 'https://testserver/external-asset-2'}
+        put_payload['href'] = settings.EXTERNAL_TEST_ASSET_URL
+        patch_payload = {'href': settings.EXTERNAL_TEST_ASSET_URL}
+
+        response = self.client.put(path, data=put_payload, content_type="application/json")
+        logger.debug(
+            "Internal asset name: %s compared to url name : %s", asset_name, put_payload['name']
+        )
+        self.assertStatusCode(200, response)
+        self.assertIn(put_payload['href'], response.json()['href'])
 
         response = self.client.patch(path, data=patch_payload, content_type="application/json")
         self.assertStatusCode(200, response)
+        self.assertIn(patch_payload['href'], response.json()['href'])
+
+    def test_asset_endpoint_put_patch_external_href(self):
+        collection_name = self.collection_external.name
+        asset_name = self.external_asset['name']
+        asset_sample = self.external_asset.copy()
+
+        path = f'/{STAC_BASE_V}/collections/{collection_name}/assets/{asset_name}'
+
+        #verify that the asset is external
+        response = self.client.get(path)
+        self.assertStatusCode(200, response)
+        self.assertTrue(response.json()['href'].startswith('http'))
+
+        put_payload = asset_sample.get_json('put')
+        put_payload['href'] = settings.EXTERNAL_TEST_ASSET_URL
+        patch_payload = {'href': settings.EXTERNAL_TEST_ASSET_URL}
 
         response = self.client.put(path, data=put_payload, content_type="application/json")
         self.assertStatusCode(200, response)
+        self.assertIn(put_payload['href'], response.json()['href'])
 
-    def test_asset_endpoint_patch_put_external_href(self):
-        collection_name = self.collection['name']
-        asset_name = 'new-external-asset.tiff'
-        asset_sample = self.asset.copy()
-        asset_sample['id'] = asset_name
-        asset_sample['name'] = asset_name
-
-        path = f'/{STAC_BASE_V}/collections/{collection_name}/assets/{asset_name}'
-        put_payload = asset_sample.get_json('put')
-        put_payload['href'] = 'https://testserver/external-asset'
-
-        response = self.client.put(path, data=put_payload, content_type="application/json")
-        self.assertStatusCode(201, response)
-
-        patch_payload = {'href': 'https://testserver/external-asset-2'}
         response = self.client.patch(path, data=patch_payload, content_type="application/json")
         self.assertStatusCode(200, response)
         self.assertIn(patch_payload['href'], response.json()['href'])
