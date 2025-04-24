69ba2b65ae3c153bb6dde037c5f794b8ab602d95
Adrien Kunysz <adrien.kunysz@swisstopo.ch>
2025-04-24 15:27:36 +0200

    Add --max-deletions* safety flags.
    This adds two flags: --max-deletions (absolute value) and
    --max-deletions-percentage (a percentage). If we detect an attempt to remove
    more than either value of Items, we fail with an error.
    
    This is useful to guard against bugs that would accidentally wipe out the
    whole database and to reduce risks of overloading the database with a request
    that might consume a lot of resources.
    
    Default values are based on the daily usage we observe in PROD. Given the
    backlog of expired Items currently in the database, the safety will trigger
    until we clean things up manually.
    
    This also forbids negative values for --min-age-hours.

--- app/stac_api/management/commands/remove_expired_items.py
+++ app/stac_api/management/commands/remove_expired_items.py
@@ -1,3 +1,4 @@
+import functools
 from datetime import timedelta
 
 from django.conf import settings
@@ -12,6 +13,13 @@ from stac_api.utils import CommandHandler
 from stac_api.utils import CustomBaseCommand
 
 
+class SafetyAbort(Exception):
+
+    def __str__(self):
+        return (f"Attempting to delete too many items:"
+                f" {self.args[1]} > {self.args[0]}.")
+
+
 class Handler(CommandHandler):
 
     def delete(self, instance, object_type):
@@ -20,14 +28,36 @@ class Handler(CommandHandler):
         else:
             instance.delete()
 
+    def _raise_if_too_many_deletions(self, max_deletions, max_deletions_pct, items_count):
+        if items_count > max_deletions:
+            exception = SafetyAbort(max_deletions, items_count)
+            self.print_error("%s", str(exception))
+            raise exception
+
+        items_deleted_pct = 100 * items_count / Item.objects.count()
+        if items_deleted_pct > max_deletions_pct:
+            exception = SafetyAbort(f"{int(max_deletions_pct)}%", f"{items_deleted_pct:.2f}%")
+            self.print_error("%s", str(exception))
+            raise exception
+
     def run(self):
         self.print_success('running command to remove expired items')
         min_age_hours = self.options['min_age_hours']
-        self.print_warning(f"deleting all items expired longer than {min_age_hours} hours")
+        max_deletions = self.options['max_deletions']
+        max_deletions_pct = self.options['max_deletions_percentage']
+        self.print_warning(
+            f"deleting no more than {max_deletions} or "
+            f"{max_deletions_pct}%% items expired for longer"
+            f" than {min_age_hours} hours"
+        )
+
         expiration = timezone.now() - timedelta(hours=min_age_hours)
 
         items = Item.objects.filter(properties_expires__lte=expiration)
         items_count = items.count()
+
+        self._raise_if_too_many_deletions(max_deletions, max_deletions_pct, items_count)
+
         assets = Asset.objects.filter(item__properties_expires__lte=expiration)
         asset_uploads = AssetUpload.objects.filter(
             asset__item__properties_expires__lte=expiration,
@@ -54,8 +84,22 @@ class Command(CustomBaseCommand):
     This command is thought to be scheduled as cron job.
     """
 
+    def _validate_int(self, candidate, min_value=None, max_value=None):
+        value = int(candidate)
+        if min_value is not None and value < min_value:
+            raise ValueError(f"{value} is less than {min_value}")
+        if max_value is not None and value > max_value:
+            raise ValueError(f"{value} is greater than {max_value}")
+        return value
+
     def add_arguments(self, parser: CommandParser) -> None:
         super().add_arguments(parser)
+        parser.register('type', 'positive_int', functools.partial(self._validate_int, min_value=0))
+        parser.register(
+            'type',
+            'percentage_int',
+            functools.partial(self._validate_int, min_value=0, max_value=100)
+        )
         parser.add_argument(
             '--dry-run',
             action='store_true',
@@ -64,9 +108,31 @@ class Command(CustomBaseCommand):
         default_min_age = settings.DELETE_EXPIRED_ITEMS_OLDER_THAN_HOURS
         parser.add_argument(
             '--min-age-hours',
-            type=int,
+            type='positive_int',
             default=default_min_age,
             help=f"Minimum hours the item must have been expired for (default {default_min_age})"
+        )
+        default_max_deletions = settings.DELETE_EXPIRED_ITEMS_MAX
+        parser.add_argument(
+            '--max-deletions',
+            type='positive_int',
+            default=default_max_deletions,
+            help=(
+                f"Maximum number of items to delete. If that number of items"
+                f" have expired, this programm will fail. Default value:"
+                f" {default_max_deletions}."
+            )
+        )
+        default_max_deletions_percentage = settings.DELETE_EXPIRED_ITEMS_MAX_PERCENTAGE
+        parser.add_argument(
+            '--max-deletions-percentage',
+            type='percentage_int',
+            default=default_max_deletions_percentage,
+            help=(
+                f"Maximum percentage of items to delete. If that percentage of"
+                f" items are expired, this program will fail."
+                f" Default value: {default_max_deletions_percentage}."
+            )
         )
 
     def handle(self, *args, **options):
