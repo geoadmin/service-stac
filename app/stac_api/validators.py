2c01d46ca3374be055c93879b1b07789d9074799
Benjamin Sugden <benjamin.sugden@swisstopo.ch>
2026-03-02 11:11:40 +0100

    Add database migration script for new TAR MediaType

--- app/stac_api/validators.py
+++ app/stac_api/validators.py
@@ -107,7 +107,7 @@ MEDIA_TYPES = [
     MediaType('application/grib', 'GRIB/GRIB2', ['.grib', '.grib2']),
     MediaType('application/x-hdf5', 'HDF5', ['.h5']),
     MediaType('application/x.aixm', 'AIXM', ['.aixm']),
-    MediaType('application/x.aixm+zip', 'Zipped AIXM', ['.zip'])
+    MediaType('application/x.aixm+zip', 'Zipped AIXM', ['.zip']),
     MediaType('application/x-tar', 'TAR', ['.tar'])
 ]
 
